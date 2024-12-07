07e4a80fddd25558f5494d79bf7a7968d6587168
szwos <szwos5@gmail.com>
2024-12-07 04:35:11 +0100

    Movement and digging changes

--- Assets/Scripts/Dig.cs
+++ Assets/Scripts/Dig.cs
@@ -8,39 +8,94 @@ public delegate void DiggingEventHandler(object sender, Vector3 diggingDestinati
 
 public class Dig : MonoBehaviour
 {
-    public Collider2D DrillCollider;
-    public GameObject DrillSelector; //has to be before DrillCollider, so that it is actually inside colliding block, not a pixel next to
+    public Collider2D DrillColliderHorizontal;
+    public Collider2D DrillColliderVertical;
+    public GameObject DrillSelectorHorizontal; //has to be before DrillCollider, so that it is actually inside colliding block, not a pixel next to
+    public GameObject DrillSelectorVertical;
     public TerrainMananger Terrain;
+    public TilemapMovement PlayerMovement;
 
     public event DiggingEventHandler Digging;
 
     private void Start()
     {
-        DrillCollider.enabled = false;
-    }
+        DrillColliderHorizontal.enabled = false;
+        DrillColliderVertical.enabled = false;    }
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.LeftControl))
+        if(Input.GetKey(KeyCode.LeftControl))
         {
-            DrillCollider.enabled = true;
+            DrillColliderHorizontal.enabled = true;
+            TryDig();
         }
 
         if (Input.GetKeyUp(KeyCode.LeftControl))
         {
-            DrillCollider.enabled = false;
+            DrillColliderHorizontal.enabled = false;
         }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            TryDig();
+            DrillColliderVertical.enabled = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            DrillColliderVertical.enabled = false;
+        }
+
+
     }
 
-    // Update is called once per frame
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void TryDig()
     {
-        if(!Input.GetKey(KeyCode.LeftControl))
+        Debug.Log($"IsGrounded: {PlayerMovement.IsGrounded}\n IsDigging: {PlayerMovement.IsDigging}");
+        if (!PlayerMovement.IsGrounded || PlayerMovement.IsDigging == 0)
         {
             return;
         }
 
-        BlockBase? block = Terrain.GetBlock(DrillSelector.transform.position);
+        GameObject selector = PlayerMovement.IsDigging switch
+        {
+            1 => DrillSelectorHorizontal,
+            2 => DrillSelectorVertical
+        };
+
+        BlockBase? block = Terrain.GetBlock(selector.transform.position);
+
+        if (block == null)
+        {
+            return;
+        }
+
+        if (block.CanDestroy())
+        {
+            Terrain.SetBlock(selector.transform.position, BlockRegistry.Air);
+            //Digging.Invoke(collision.gameObject, selector.transform.position);
+            PlayerMovement.DigToPosition(selector.transform.position);
+
+        }
+    }
+
+
+    // Update is called once per frame
+    private void OnTrigger2D(Collider2D collision)
+    {
+        Debug.Log($"IsGrounded: {PlayerMovement.IsGrounded}\n IsDigging: {PlayerMovement.IsDigging}");
+        if (!PlayerMovement.IsGrounded || PlayerMovement.IsDigging == 0) 
+        {
+            return;
+        }
+
+        GameObject selector = PlayerMovement.IsDigging switch
+        {
+            1 => DrillSelectorHorizontal,
+            2 => DrillSelectorVertical
+        };
+
+        BlockBase? block = Terrain.GetBlock(selector.transform.position);
 
         if(block == null)
         {
@@ -49,8 +104,8 @@ public class Dig : MonoBehaviour
 
         if(block.CanDestroy())
         {
-            Terrain.SetBlock(DrillSelector.transform.position, BlockRegistry.Air);
-            Digging.Invoke(collision.gameObject, DrillSelector.transform.position);
+            Terrain.SetBlock(selector.transform.position, BlockRegistry.Air);
+            Digging.Invoke(collision.gameObject, selector.transform.position);
         }
 
     }
--- Assets/Scripts/TilemapMovement.cs
+++ Assets/Scripts/TilemapMovement.cs
@@ -2,12 +2,63 @@ using System;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+//TOOD: flipowanie postaci
+//TOOD: stweakowaæ lerpa, bo po kopaniu sie jakos na chwile zatrzymuje zanim zacznie kopac nastepny (ale to nie jest wazne bardzo)
+//TODO: posprzatac tu moze, chociaz wsm wyjebane, to jest gamejam xd
+
 public class TilemapMovement : MonoBehaviour
 {
     [SerializeField]
     public Tilemap Tilemap;
     public Rigidbody2D SelfRb;
+    public GameObject GroundedDetector;
     public Dig DigBehaviour;
+    public float DiggingSpeed;
+    public float MaxGroundedDistance = 0.1f;
+    
+
+    private Vector3 _diggingStartPosition;
+    private Vector3 _diggingEndPosition;
+    private Vector3 _movementDirection = Vector3.right;
+    private float _diggingStartTime;
+    private float _diggingDistance;
+    private bool _canMove = true;
+
+    public int IsDigging
+    {
+        //0 - not digging
+        //1 - horizontaly
+        //2 - vertically
+        get
+        {
+            if (Input.GetKey(KeyCode.LeftControl))
+            {
+                return 1;
+            }
+            else if(Input.GetKey(KeyCode.DownArrow))
+            {
+                return 2;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+
+    public bool IsGrounded
+    {
+        get
+        {
+            RaycastHit2D hit = Physics2D.Raycast(GroundedDetector.transform.position, Vector2.down, MaxGroundedDistance, LayerMask.GetMask("Ground"));
+            Debug.DrawRay(GroundedDetector.transform.position, transform.TransformDirection(Vector3.down) * 1, Color.green);
+            if(hit.distance > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,33 +69,60 @@ public class TilemapMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-
-        //TODO: use fuel
-        if (Input.GetKey(KeyCode.RightArrow))
+        if(Input.GetKeyDown(KeyCode.LeftArrow)) 
         {
-            SelfRb.AddForce(new Vector2(250f, 0));
+            _movementDirection = Vector3.left;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            _movementDirection = Vector3.right;
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow))
+
+        if (_canMove)
         {
-            SelfRb.AddForce(new Vector2(-250f, 0));
-        }
-        
-        if (Input.GetKey(KeyCode.Space))
+
+
+            //TODO: use fuel
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                SelfRb.AddForce(new Vector2(250f, 0));
+            }
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                SelfRb.AddForce(new Vector2(-250f, 0));
+            }
+            if (Input.GetKey(KeyCode.Space))
+            {
+                SelfRb.AddForce(new Vector2(0, 500f));
+            }
+        } else
         {
-            SelfRb.AddForce(new Vector2(0, 500f));
+            float distanceCovered = (Time.time - _diggingStartTime) * DiggingSpeed;
+            float fractionOfMovement = distanceCovered / _diggingDistance;
+            transform.position = Vector3.Lerp(_diggingStartPosition, _diggingEndPosition, fractionOfMovement);
+            if(distanceCovered >= _diggingDistance - 0.01f)
+            {
+                _canMove = true;
+            }
         }
+
     }
 
     private void OnDigging(object sender, Vector3 diggingDestination)
     {
-        transform.position = diggingDestination;
+        //transform.position = diggingDestination;
+        DigToPosition(diggingDestination);
+    }
+    
+    public void DigToPosition(Vector3 destination)
+    {
+        _diggingStartPosition = transform.position;
+        _diggingEndPosition = destination;
+        _diggingStartTime = Time.time;
+        _diggingDistance = Vector3.Distance(_diggingStartPosition, _diggingEndPosition);
+        _canMove = false;
     }
 
-    /*void Move(Vector2Int direction)
-    {
-        var cellPos = tilemap.WorldToCell(transform.position);
-
-        transform.position = tilemap.CellToWorld(cellPos + (Vector3Int)direction);
-    }*/
 }
