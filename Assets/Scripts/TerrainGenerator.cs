48ce58ece9dc871166c7523c55c93f69c500c36b
szwos <szwos5@gmail.com>
2024-12-07 01:45:53 +0100

    TileManager changes

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -1,55 +1,34 @@
 using System;
 using System.Collections.Generic;
 using DefaultNamespace;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class TerrainGenerator : MonoBehaviour
 {
-    public Tilemap tilemap;
-    // public TileBase[] tiles;
 
-    public TileBase stone;
-    public TileBase ore;
-    
+    public TerrainMananger TerrainMananger;
+
+    public int Width = 100;
+    public int Height = 200;
+
     private Dictionary<BlockId, TileBase> blocks = new();
     
     void Start()
-    {
-        blocks.Add(BlockRegistry.Stone, stone);
-        blocks.Add(BlockRegistry.Ore, ore);
-        
-        Debug.Log(tilemap);
+    {        
         BlockId[,] mapData = GenerateMapData();
-        FillTilemap(mapData);
+        TerrainMananger.Init(mapData);
+
     }
 
     BlockId[,] GenerateMapData()
     {
         TerrainGen terrainGen = new()
         {
-            Width = 256,
-            Height = 512,
+            Width = Width,
+            Height = Height,
         };
         return terrainGen.Generate();
-    }
-
-    void FillTilemap(BlockId[,] mapData)
-    {
-        int width = mapData.GetLength(0);
-        int height = mapData.GetLength(1);
-        
-        for (int x = 0; x < width; x++)
-        {
-            for (int y = 0; y < height; y++)
-            {
-                var id = mapData[x, y];
-                
-                if (blocks.TryGetValue(id, out TileBase tile))
-                {
-                    tilemap.SetTile(new Vector3Int(x, -y, 0), tile);
-                }
-            }
-        }
     }
 }
--- Assets/Scripts/TerrainMananger.cs
+++ Assets/Scripts/TerrainMananger.cs
@@ -1,9 +1,64 @@
-﻿namespace DefaultNamespace
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using static Unity.Collections.AllocatorManager;
+
+namespace DefaultNamespace
 {
-    public class TerrainMananger
+    public class TerrainMananger : MonoBehaviour
     {
-        public BlockId[,] Blocks { get; set; }
-        
-        
+        public TileBase stone;
+        public TileBase ore;
+
+        private Tilemap _tilemap;
+        private BlockId[,] _blockMap;
+        private Dictionary<BlockId, TileBase> _blockDictionary = new();
+
+        public void Start()
+        {
+            _tilemap = GetComponentInChildren<Tilemap>();
+        }
+
+        public void Init(BlockId[,] mapData)
+        {
+            _blockDictionary.Add(BlockRegistry.Stone, stone);
+            _blockDictionary.Add(BlockRegistry.Ore, ore);
+
+            _blockMap = mapData;
+            FillTilemap(mapData);
+        }
+
+        void FillTilemap(BlockId[,] mapData)
+        {
+            int width = mapData.GetLength(0);
+            int height = mapData.GetLength(1);
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    var id = mapData[x, y];
+
+                    if (_blockDictionary.TryGetValue(id, out TileBase tile))
+                    {
+                        _tilemap.SetTile(new Vector3Int(x, -y, 0), tile);
+                    }
+                }
+            }
+        }
+
+        public void SetBlock(Vector3Int position, BlockId blockId)
+        {
+            _tilemap.SetTile(position, _blockDictionary[blockId]);
+        }
+
+        public BlockBase GetBlock(Vector3 position)
+        {
+            Vector3Int cellCoords = _tilemap.WorldToCell(position);
+            var blockId = _blockMap[cellCoords.x, cellCoords.y];
+            return BlockRegistry.Blocks[blockId];
+        }
+
     }
 }
