b3c7a0f77a9899b1236865072fe6604138fb9e34
MKKL <Mkkl3301@gmail.com>
2024-12-09 03:08:41 +0100

    Add end game scene

---
+++ Assets/Scripts/GameEndDetector.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DefaultNamespace
+{
+    public class GameEndDetector : MonoBehaviour
+    {
+        public GameObject grid;
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                EndGame();
+            }
+        }
+
+        private void EndGame()
+        {
+            DontDestroyOnLoad(grid);
+            SceneManager.LoadScene((int)ScenesEnum.GAME_END_SCENE);
+        }
+        
+    }
+}
